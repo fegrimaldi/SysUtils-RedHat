c4e300900c5d171b1b176002bb3f1630f90c5099
Fabricio Grimaldi <34732205+fegrimaldi@users.noreply.github.com>
2020-09-07 14:57:54 -0400

    Fixed miniconda install script errors.

--- prepare_shared_miniconda.sh
+++ prepare_shared_miniconda.sh
@@ -11,6 +11,8 @@ END_COLOR='\e[0m'
 
 printf "${YELLOW}Installing Miniconda to /usr/share/miniconda3.${END_COLOR}\n"
 sudo ./Miniconda3-latest-Linux-x86_64.sh -b -p /usr/share/miniconda3 && \
+sudo chown root:webdevs -R /usr/share/miniconda3
+sudo chmod 775 -R /usr/share/miniconda3
 
 printf "${YELLOW}Activating Miniconda3.${END_COLOR}\n"
 source /usr/share/miniconda3/bin/activate && \
